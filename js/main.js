f2e27b34e3eddc0d455d21d1fc12653ede6802a0
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-08 20:39:30 +0200

    add: calc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
-//QUESTIONS
-const KM = prompt('How many km should you travel on the train?');
-const AGE = prompt('How old are you?');
+const aKM = prompt('How many km should you travel on the train?');
+const aAGE = prompt('How old are you?');
+
+//in numeri
+const klm = Number(aKM);
+const age = parseInt(aAGE);
+const PRICE = klm * 0.21;
+
+const PRICE18 = PRICE - (PRICE * 0.20);
+const PRICE65 = PRICE - (PRICE * 0.40);
+
+if (age < 18) {
+    console.log(PRICE18);
+} else if (age >= 65) {
+    console.log(PRICE65);
+} else {
+    console.log(PRICE);
+}
+
+
+//PRICE
+// const PRICE18 = ((aKM * 0, 21) - 20 %);
+// const PRICE65 = ((aKM * 0, 21) - 40 %);
+// const PRICE = aKM * 0.21;
+
+// //AGE
+// let age = aAGE;
+// if (age <<= 18) {
+//     console.log(PRICE18)
+// }
+// if (age >>= 65) {
+//     console.log(PRICE65)
+// }
+// else {
+//     console.log(PRICE)
+// }
+
+
+
+
+//trasformo risposta in numero intero
+//const km = Number(parseInt(prompt('How many km should you travel on the train?')));

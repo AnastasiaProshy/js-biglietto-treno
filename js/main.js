c76ee2de1fdf3debfcb472b717c456edfa0ee588
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-12 18:24:53 +0200

    change: main-algoritm

--- js/main.js
+++ js/main.js
@@ -1,42 +1,38 @@
-const aKM = prompt('How many km should you travel on the train?');
-const aAGE = prompt('How old are you?');
+const userKM = prompt('How many km should you travel on the train?');
+const userAGE = prompt('How old are you?');
 
-//in numeri
-const klm = parseInt(aKM);
-const age = parseInt(aAGE);
-const PRICE = klm * 0.21;
+//OUTPUT STRING
+const output = `
+***user data***
+travel: ${userKM}
+old: ${userAGE}
+***************
+`;
+console.log(output);
 
-const PRICE18 = PRICE - (PRICE * 0.20);
-const PRICE65 = PRICE - (PRICE * 0.40);
+//STRING TO NUMBERS
+const klm = parseInt(userKM);
+const age = parseInt(userAGE);
+
+//MAIN ALGORITM
+const DefaultPrice = (klm * 0.21).toFixed(2);
+const priceUnder18 = (DefaultPrice - (DefaultPrice * 0.20)).toFixed(2);
+const priceOver65 = (DefaultPrice - (DefaultPrice * 0.40)).toFixed(2);
+
+//OUTPUT
+const string = `The total price of your trip is `
 
 if (age <= 18) {
-    console.log(PRICE18);
-} else if (age >= 65) {
-    console.log(PRICE65);
-} else {
-    console.log(PRICE);
+    finalResult = string + priceUnder18 + `€. Congratulations, you've received a 20% discount for under 19 years.`
+    console.log(priceUnder18);
+}
+else if (age >= 65) {
+    finalResult = string + priceOver65 + `€. Congratulations, you've received a 40% discount for over 64 years.`
+    console.log(priceOver65);
+}
+else {
+    finalResult = string + DefaultPrice + `€. Unfortunately, there are no discounts available for you.`
+    console.log(DefaultPrice);
 }
 
-
-//PRICE
-// const PRICE18 = ((aKM * 0, 21) - 20 %);
-// const PRICE65 = ((aKM * 0, 21) - 40 %);
-// const PRICE = aKM * 0.21;
-
-// //AGE
-// let age = aAGE;
-// if (age <<= 18) {
-//     console.log(PRICE18)
-// }
-// if (age >>= 65) {
-//     console.log(PRICE65)
-// }
-// else {
-//     console.log(PRICE)
-// }
-
-
-
-
-//trasformo risposta in numero intero
-//const km = Number(parseInt(prompt('How many km should you travel on the train?')));
+document.getElementById('PRICE').innerText = finalResult;

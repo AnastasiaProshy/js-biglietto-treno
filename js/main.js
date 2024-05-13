716e3292d1604be95f1205258bbb8b809ffc9159
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-13 21:57:36 +0200

    updating

--- js/main.js
+++ js/main.js
@@ -16,17 +16,17 @@ const age = parseInt(userAGE);
 
 //MAIN ALGORITM
 const DefaultPrice = (klm * 0.21).toFixed(2);
-const priceUnder18 = (DefaultPrice - (DefaultPrice * 0.20)).toFixed(2);
-const priceOver65 = (DefaultPrice - (DefaultPrice * 0.40)).toFixed(2);
 
 //OUTPUT
 const string = `The total price of your trip is `
 
 if (age <= 18) {
+    const priceUnder18 = (DefaultPrice - (DefaultPrice * 0.20)).toFixed(2);
     finalResult = string + priceUnder18 + `€. Congratulations, you've received a 20% discount for under 19 years.`
     console.log(priceUnder18);
 }
 else if (age >= 65) {
+    const priceOver65 = (DefaultPrice - (DefaultPrice * 0.40)).toFixed(2);
     finalResult = string + priceOver65 + `€. Congratulations, you've received a 40% discount for over 64 years.`
     console.log(priceOver65);
 }
@@ -35,4 +35,6 @@ else {
     console.log(DefaultPrice);
 }
 
+
 document.getElementById('PRICE').innerText = finalResult;
+
